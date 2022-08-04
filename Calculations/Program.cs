0b970d7da1b07ddf8e02baa7276e5817a4313c7e
Andrzej Labecki <andrzej.labecki@hotmail.com>
2022-08-03 21:58:03 -0400

    test2

--- Calculations/Program.cs
+++ Calculations/Program.cs
@@ -12,12 +12,19 @@ namespace Calculations
             // '/' divide
             // 'Math.Pow(x, y)' y power of x
 
+            Test1();
+            Test2();
+        }
+
+        static void Test1()
+        {
+            Console.WriteLine("Test #1");
             Console.WriteLine("Calculate - (Pow((3.75/5), 2)  + 4 + 3/8) - 0.625");
 
-            double a = (double)3.75/(double)5;
+            double a = (double)3.75 / (double)5;
             //double b = Math.Pow(a, 2);
-            double b = a*a;
-            double c = b + 4 + (double)3/(double)8;
+            double b = a * a;
+            double c = b + 4 + (double)3 / (double)8;
             double d = c - (double)0.625;
 
             double answer = 4 + (double)5 / (double)16;
@@ -28,7 +35,27 @@ namespace Calculations
             Console.WriteLine("Test = " + test);
             Console.WriteLine("Press any button to exit...");
             Console.ReadKey();
+        }
 
+        static void Test2()
+        {
+            Console.WriteLine("Test #2");
+            Console.WriteLine("Calculate - (Pow((3.75/5), 2)  + 4 + 3/8) - 7/8");
+
+            double a = (double)3.75 / (double)5;
+            //double b = Math.Pow(a, 2);
+            double b = a * a;
+            double c = b + 4 + (double)3 / (double)8;
+            double d = c - (double)7 / (double)8;
+
+            double answer = 4 + (double)1 / (double)16;
+            double test = 1 / 16;
+
+            Console.WriteLine("Result = " + d);
+            Console.WriteLine("Answer = " + answer);
+            Console.WriteLine("Test = " + test);
+            Console.WriteLine("Press any button to exit...");
+            Console.ReadKey();
         }
     }
 }
